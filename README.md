34c2505312cca03ef16ae5c3d16500e7b6d9ecfc
Jesfery <sean.shanahan@bigpond.com>
2018-02-07 23:45:41 +1100

    Committing  latest version

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple discord.js bot to manage Voice Channels.
 
 Presently, it only updates the name of a voice channel to represent the game that the majority of members in the channel are playing.
+
+Will also set the userLimit of an occupied channel by vote.
---
+++ listeners/channelState.js
@@ -0,0 +1,96 @@
+function resetUserLimit(voiceChannel) {
+    if (voiceChannel.type !== 'voice' || voiceChannel.name.indexOf('Voice #') === -1) {
+        return;
+	}
+	
+	voiceChannel.edit({
+        userLimit: 0
+    });
+}
+
+
+function updateChannelGame (channel) {
+    let games = {},
+        max = 0, gameName, channelName;
+
+    if (channel.type !== 'voice' || channel.name.indexOf('Voice #') === -1) {
+        return;
+    }
+
+    channel.members.forEach(member => {
+        console.log(member.displayName);
+
+        let name = presence.game.name;
+
+        if (name && name !== '') {
+            console.log(name);
+
+            if (games[name] !== undefined) {
+                games[name]++;
+            } else {
+                games[name] = 1;                
+            }
+        }
+
+    });
+
+    for (let n in games) {
+        if (games.hasOwnProperty(n)) {
+            console.log(n + ': ' + games[n]);
+
+            if (games[n] > max) {
+                max = games[n];
+                gameName = n;
+            }
+        }
+    }
+
+    channelName = channel.name.split('(')[0].trim();
+
+    if (max > 0) {
+        channelName = channelName + ' (' + gameName + ')';
+    }
+
+	channel.setName(channelName);
+}
+
+module.exports = {
+    init: function(client) {
+
+        //Update channel state at startup
+        client.guilds.forEach(guild => {
+            guild.channels.filter(channel => {
+                return channel.type === 'voice';
+            }).forEach(channel => {
+                updateChannelGame(channel);
+            });
+
+        });
+
+        client.on('voiceStateUpdate', (oldMember, newMember) => {
+            let newUserChannel = newMember.voiceChannel,
+                oldUserChannel = oldMember.voiceChannel;
+        
+            if(oldUserChannel !== undefined) {
+                updateChannelGame(oldUserChannel);
+                if (oldUserChannel.members.size === 0) {
+                    resetUserLimit(oldUserChannel);
+                }
+            } 
+            if(newUserChannel !== undefined){
+                updateChannelGame(newUserChannel);
+            }
+        });
+        
+        client.on('presenceUpdate', (oldMember, newMember) => {
+            let newUserGame = newMember.presence.game,
+                oldUserGame = oldMember.presence.game,
+                newUserChannel = newMember.voiceChannel;
+        
+            if (newUserChannel && (!newUserGame || !oldUserGame || !newUserGame.equals(oldUserGame))) {
+                updateChannelGame(newUserChannel);
+            }
+        });        
+        
+    }
+};
