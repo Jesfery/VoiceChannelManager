8c2ee4398434016cccda4fd88792eb45254ccfa7
Jesfery <sean.shanahan@bigpond.com>
2018-02-08 01:06:53 +1100

    Revert "Revert "Committing  latest version""
    This reverts commit acecf9839ae37bc985d90f8a4f8fbef2ac084cd6.

---
+++ commands/setmax.js
@@ -0,0 +1,51 @@
+const vote = require('../vote.js');
+
+module.exports = {
+	name: 'setmax',
+	description: 'Set the maximum number of users that can connect to your voice channel.',
+    usage: 'maxUsers',
+    cooldown: 5,	
+    guildOnly: true,
+
+    execute(message, args) {
+        return new Promise((resolve, reject) => {
+            let maxInt = Number.parseInt(args[0]),
+                subject = '',
+                voiceChannel = message.member.voiceChannel,
+                targetUsers = [],
+                userCount = voiceChannel.members.size;
+        
+            if (voiceChannel === undefined) {
+                reject('User not connected to a voice channel');
+                return;
+            }
+        
+            if (Number.isNaN(maxInt) || maxInt < 0 || maxInt >= 100) {
+                reject('Invalid user limit: ' + maxInt);
+                return;
+            }
+
+            voiceChannel.members.forEach(function(member) {
+                if (member.id !== message.member.id) {
+                    targetUsers.push(member);
+                }
+            });
+
+            if (userCount > 1) {
+                vote.voteOn('Set user limit of ' + voiceChannel.name + ' to ' + maxInt + '? Please vote using the reactions below.', message.channel, {
+                    targetUsers: targetUsers
+                }).then(results => {
+                    if (((results.agree.count + 1) / userCount) > 0.5) { //+1 for requesting user
+                        voiceChannel.edit({
+                            userLimit: maxInt
+                        });
+                    }
+                });
+            } else {
+                voiceChannel.edit({
+                    userLimit: maxInt
+                });
+            }
+        });
+	}
+};
