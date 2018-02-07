b6ee798d921c4da7af10d95e963847f20e7d0894
Jesfery <sean.shanahan@bigpond.com>
2018-02-07 21:14:08 +1100

    Added setmax back

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const Discord = require('discord.js');
 
 // Discord Token, located in the Discord application console - https://discordapp.com/developers/applications/me/
-const { token } = require('./config.json');
+const { token, prefix } = require('./config.json');
 
 // Instance of Discord to control the bot
 const bot = new Discord.Client();
@@ -77,8 +77,109 @@ function updateChannelGame (channel) {
         bitrate = bitrate * 1000;
     }
 
-    channel.edit({
+    return channel.edit({
         name: channelName,
+        bitrate: bitrate
+    });
+}
+
+function setMax(voiceChannel, textChannel, max, cb, errCb) {
+    var maxInt = Number.parseInt(max),
+        bitrate = voiceChannel.bitrate,
+        agree = '✅',
+        disagree = '❌',
+        mesage;
+
+    //Do this because discord.js has some issue.
+    if (bitrate <= 96) {
+        bitrate = bitrate * 1000;
+    }
+
+    if (voiceChannel === undefined) {
+        errCb('User not connected to a voice channel');
+        return;
+    }
+
+    if (voiceChannel.name.indexOf('Voice #') !== 0) {
+        errCb('Cannot be used in this channel');
+        return;
+    }
+
+    if (Number.isNaN(maxInt) || maxInt < 0 || maxInt >= 100) {
+        errCb('Invalid user limit: ' + max);
+        return;
+    }
+
+    if (voiceChannel.members.size === 1) {
+        voiceChannel.edit({
+            userLimit: maxInt,
+            bitrate: bitrate
+        }).then(c => {
+            cb('New user limit set');
+        });        
+        return;
+    }
+
+    message = [];
+    voiceChannel.members.forEach(member => {
+        message.push(member.toString());
+    });
+    message = message.join(' ');
+    message += '\n\nSet user limit of ' + voiceChannel.name + ' to ' + maxInt + '? Please vote using the reactions below.';
+
+    //Vote
+    textChannel.send(message).then(msg => {
+        message = msg;
+        return message.react(agree);
+    }).then(function() {
+        return message.react(disagree);
+    }).then(function() {
+        return message.awaitReactions(function(reaction) {
+            return reaction.emoji.name === agree || reaction.emoji.name === disagree;
+        }, {
+            time: 10000
+        });
+    }).then(function(reactions) {
+        var changeOked = false;
+
+        if (reactions.get(agree)) {
+            changeOked = true;
+            voiceChannel.members.forEach(function(member) {
+                changeOked = changeOked && (reactions.get(agree).users.get(member.id) !== undefined);
+            });
+        }
+
+        if (changeOked) {
+            voiceChannel.edit({
+                userLimit: maxInt,
+                bitrate: bitrate
+            }).then(c => {
+                cb('New user limit set');
+            });
+        } else {
+            errCb('Vote failed');
+        }
+    });
+
+}
+
+/**
+ * Resets voice channel user limit to 0
+ * @param {VoiceChannel} voiceChannel the voice channel
+ */
+function resetUserLimit(voiceChannel) {
+    var bitrate = voiceChannel.bitrate;
+
+    if (voiceChannel.type !== 'voice' || voiceChannel.name.indexOf('Voice #') !== 0) {
+        return;
+    }
+
+    if (bitrate <= 96) {
+        bitrate = bitrate * 1000;
+    }
+
+    voiceChannel.edit({
+        userLimit: 0,
         bitrate: bitrate
     });
 }
@@ -96,10 +197,16 @@ bot.on('ready', function() {
 //Called when a user enters or leaves a voice channel.
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
     var newUserChannel = newMember.voiceChannel,
-        oldUserChannel = oldMember.voiceChannel;
+        oldUserChannel = oldMember.voiceChannel,
+        resetUsers;
 
     if(oldUserChannel !== undefined) {
-        updateChannelGame(oldUserChannel);
+        resetUsers = (oldUserChannel.members.size === 0);
+        updateChannelGame(oldUserChannel).then(function() {
+            if (resetUsers) {
+                resetUserLimit(oldUserChannel);
+            }
+        });
     } 
     if(newUserChannel !== undefined){
         updateChannelGame(newUserChannel);
@@ -115,6 +222,44 @@ bot.on('presenceUpdate', (oldMember, newMember) => {
 
     if (newUserChannel && presenceChanged) {
         updateChannelGame(newUserChannel);
+    }
+});
+
+// Listen for messages sent to the server where the bot is located
+bot.on('message', message => {
+    var author = message.author,
+        textChannel, voiceChannel, max;
+
+    // So the bot doesn't reply to iteself
+    if (message.author.bot) return;
+
+    // Check if the message starts with the '!' trigger
+    if (message.content.indexOf(prefix) === 0) {
+
+        // Get the user's message excluding the '!'
+        var text = message.content.substring(1);
+
+        text = text.split(' ');
+
+        if (text[0] === 'setmax' || text[0] === 'clearmax') {
+
+            textChannel = message.channel;
+            voiceChannel = message.member.voiceChannel;
+            
+            if (text[0] === 'clearmax') {
+                max = 0;
+            } else {
+                max = text[1];
+            }
+
+            setMax(voiceChannel, textChannel, max, msg => {
+                message.channel.send(msg);
+            }, error => {
+                message.channel.send(error);
+            });
+
+        }
+
     }
 });
 
