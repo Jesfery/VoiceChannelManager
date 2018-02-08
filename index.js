b94fb0962aa5860b9b570c8c7ba2d58fb2aabe0e
Jesfery <sean.shanahan@bigpond.com>
2018-02-08 12:12:56 +1100

    Added description to index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 /*jshint esversion: 6 */
 
+/**
+ * Discord Voice Channel Manager
+ * A simple discord.js bot to manage Voice Channels.
+ *
+ * Presently, it only updates the name of a voice channel to represent the game that the majority of members in the channel are playing.
+ *
+ * Will also set the userLimit of an occupied channel by vote.
+ *
+ * Comand handling based on https://github.com/discordjs/guide/tree/master/code_samples/command-handling/dynamic-commands
+ */
 const Discord = require('discord.js');
 const { token } = require('./config.json');
 
